59e6dc95173879e955caecae9ca641624219a4d0
Caleb <17859984+C359@users.noreply.github.com>
2023-03-27 11:34:42 -0700

    Complete Exercises 01 & 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(compString, num) {
 
+    if (num < 0) {
+        return 'ERROR';
+    }
+
+    finalString = ''
+
+
+    for (i=0; i<num; i++){
+        finalString += compString;
+    }
+
+    return finalString;
 };
 
 // Do not edit below this line
