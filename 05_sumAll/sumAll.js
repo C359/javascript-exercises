036727bc594eb2cf452f268e1feecefc52dda8cb
Caleb <17859984+C359@users.noreply.github.com>
2023-03-27 14:01:20 -0700

    Complete Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if(!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+        return 'ERROR';
+    }
+    startNum = 0;
+    endNum = 0;
+    if (a < b) {
+        startNum = a;
+        endNum = b;
+    } else {
+        startNum = b;
+        endNum = a;
+    }
+
+    total = startNum + endNum;
+    diff = endNum - startNum;
+
+    for (i = 1; i < diff; i++) {
+        total += startNum + i;
+    }
+    return total;
 
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+
+//identify the smaller number and select it as start
+//start a total that begins as 0
+//start a value that tracks the increasing number
+//in a loop add 1 to start value until end value is reached, each loop add increasing number, and i 
