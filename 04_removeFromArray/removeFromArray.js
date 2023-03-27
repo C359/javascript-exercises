dd39e23cdc4f5e90d312601b899a2116b77509ed
Caleb <17859984+C359@users.noreply.github.com>
2023-03-27 13:28:43 -0700

    Complete Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(startArray) {
+    argArr = Array.from(arguments);
+    argArr = argArr.slice(1);
+    newArr = startArray;
+    lenCount = newArr.length;
+    
+    for(i = 0; i < lenCount; i++) {
+        if (argArr.includes(newArr[i])) {
+            console.log(argArr);
+            removed = newArr.splice(i,1);
+            lenCount--;
+            i--;
+        }
+    }
 
+    return newArr;
 };
 
 // Do not edit below this line
