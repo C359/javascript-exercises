bf5fbb6299d5818869312315abc561129fab36fb
Caleb <17859984+C359@users.noreply.github.com>
2023-03-31 12:01:52 -0700

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(retInd) {
+    let fibArray = [1,1]
+    for (let i = 2; i < +retInd; i++) {
+        fibArray[i] = fibArray[i-1] + fibArray[i-2];
+    }
+    return retInd >= 0 ? fibArray[+retInd - 1]: 'OOPS';
 };
 
 // Do not edit below this line
