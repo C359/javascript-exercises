2717655be9d34037d8942900121cf37ecbf06264
Caleb <17859984+C359@users.noreply.github.com>
2023-03-31 11:15:30 -0700

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (testStr) {
+    alphOnly = Array.from(testStr).filter(char => char.match(/[a-z]/i));
+    return alphOnly.join('').toUpperCase() === alphOnly.reverse().join('').toUpperCase();
 };
 
 // Do not edit below this line
