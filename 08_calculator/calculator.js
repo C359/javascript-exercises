c5b5c25dc5768d88d657e231083e5003d69fe8c8
Caleb <17859984+C359@users.noreply.github.com>
2023-03-30 17:44:33 -0700

    Complete execise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,24 @@
-const add = function() {
-	
+const add = (a,b) => (0 + a + b);
+
+const subtract = (a,b) => (0 + a  - b);
+
+const sum = (valArr) => {
+  return valArr.reduce((total, val) => total + val, 0);
 };
 
-const subtract = function() {
-	
+
+const multiply = (valArr) => {
+  return valArr.reduce((total, val) => total * val, 1);
 };
 
-const sum = function() {
-	
-};
+const power = (a,b) => (a ** b);
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = (a) => {
+  let initArray = [...Array(a).keys()];
+  let iterArray = initArray.map(val => val + 1);
+  console.log(iterArray);
+  return iterArray.reduce((total, val) => total * val, 1);
+  
 };
 
 // Do not edit below this line
